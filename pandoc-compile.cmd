48e2c999ecc7ced9ba50b288b94fa016357be4bf
Matthias Mehrtens <matthias.mehrtens@veeam.com>
2024-01-26 13:55:40 +0100

    simplified formulas

--- pandoc-compile.cmd
+++ pandoc-compile.cmd
@@ -9,8 +9,5 @@ set mermaid=%APPDATA%\npm\mermaid-filter.cmd
 if exist "%out-html%" del "%out-html%"
 
 
-:: create temporary markdown
-"%pandoc_bin%" -F %mermaid% %srcfile% -s -t markdown -o tmp.md
-
 :: create html output
-"%pandoc_bin%" --mathjax --highlight-style=breezeDark tmp.md -s -t html -c .\water.css -o %out-html%
+"%pandoc_bin%" --mathjax --highlight-style=breezeDark %srcfile% -s -t html -c .\water.css -o %out-html%
